e3ba3964347378a585fcae7c9f4270c9ad13300e
Yato20XX <148939420+Yato20XX@users.noreply.github.com>
2023-11-09 21:53:44 +0200

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,18 +1,4 @@
-let login = document.getElementById('login');
-let pswd = document.getElementById('pswd');
-let reg = document.getElementById('reg');
+const fs = require('fs');
 
-let Users = [];
-
-function addUser(){
-	user = {};
-	user.login = login.value;
-	user.password = pswd.value;
-
-	Users.push(user);
-
-	localStorage.setItem('Users', JSON.stringify(Users));
-	console.log(Users);
-}
-
-reg.addEventListener('click', addUser);
+const data = fs.readFileSync('lol.json');
+console.log(data);
